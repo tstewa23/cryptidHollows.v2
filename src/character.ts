715ff3ac9c0ab8c2869114ce8c7e5293d8bb6e68
taylor stewart <stewarttaylor234@gmail.com>
2025-05-16 01:08:21 -0500

    character moves to where you click

--- src/character.ts
+++ src/character.ts
@@ -1,6 +1,10 @@
 import * as ex from 'excalibur';
 
 export class Character extends ex.Actor {
+    private target: ex.Vector | null = null;
+    private speed: number = 100; // pixels per second
+    private xTurn: boolean = true;
+
     constructor() {
         super({
             x: 30,
@@ -9,5 +13,41 @@ export class Character extends ex.Actor {
             height: 30,
             color: ex.Color.fromRGB(100, 50, 50, 255)
         });
+    };
+
+    moveTo(pos: ex.Vector) {
+        this.target = pos;
     }
+
+    onPreUpdate(engine: ex.Engine, delta: number) {
+        if (this.target) {
+            const dir = this.target.sub(this.pos);
+            const distance = dir.magnitude;
+
+            if (distance < 2) {
+                this.vel = ex.Vector.Zero;
+                this.target = null;
+            } else {
+                this.vel = dir.normalize().scale(this.speed);
+            }
+            // } else {
+            //     const slope = dir.normalize().scale(this.speed);
+            //     if (this.xTurn) {
+            //         this.vel = new ex.Vector(slope.x, 0);
+            //         this.xTurn = false;
+            //     } else if (!this.xTurn) {
+            //         this.vel = new ex.Vector(0, slope.x);
+            //         this.xTurn = true;
+            //     }
+            // }
+        }
+    }
+
+    override onPostUpdate(engine: ex.Engine): void {
+        engine.input.pointers.primary.on("down", (evt) => {
+            const targetPos = evt.worldPos; // Position in world space
+            this.moveTo(targetPos);
+        });
+    };
+
 };
