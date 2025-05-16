461bd36e155a96ed61f0caf8d45735238711a683
taylor stewart <stewarttaylor234@gmail.com>
2025-05-16 00:01:27 -0500

    fills in main and level files with basics

---
+++ src/level.ts
@@ -0,0 +1,7 @@
+import * as ex from 'excalibur'
+
+export class Level extends ex.Scene {
+    override onInitialize(engine: ex.Engine): void {
+        console.log('and scene')
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import * as ex from 'excalibur'
 
 
 const game = new ex.Engine({
-    width: 400,
-    height: 500,
-    backgroundColor: ex.Color.fromHex("#54C0CA"),
+    width: 600,
+    height: 400,
+    backgroundColor: ex.Color.fromRGB(100, 120, 105, 255),
     pixelArt: true,
     pixelRatio: 2,
     displayMode: ex.DisplayMode.FitScreen
